1946f41742aa53575f8b52d47502c6e3833b4b89
isayeu <63258816+isayeu@users.noreply.github.com>
2023-08-05 12:01:09 +1100

    Add files via upload

--- buildozer.spec
+++ buildozer.spec
@@ -9,11 +9,16 @@ requirements = python3,Cython==0.29.36,kivy,numpy
 #requirements = python3,Cython==3.0.0,kivy
 python_version = 3.8
 cython.language_level = 3
-version = 2.0.2
+version = 2.9.4
+# Release signing configuration
+android.p4a_release_keyalias = andrei
+android.p4a_release_keystore = my-release-key.keystore
+android.p4a_release_keystore_pass = my_keystore_vfcnth
+android.p4a_release_keyalias_pass = my_keyalias_vfcnth
 #__version__ = "0.1.2"
 #requirements.source.kivy = ~/kivy-android/kivy
 orientation = portrait
-fullscreen = 1
+fullscreen = 0
 # (int) Target Android API, should be as high as possible.
 android.api = 30
 android.minapi = 21
--- main.py
+++ main.py
@@ -4,132 +4,94 @@ import sqlite3
 import numpy as np
 from kivy.app import App
 from kivy.lang import Builder
-#from scipy.interpolate import interp1d
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
-
+from kivy.uix.tabbedpanel import TabbedPanel
 
 Builder.load_file("opt.kv")
 
-
 class UpperCaseTextInput(TextInput):
     def insert_text(self, substring, from_undo=False):
-        # Use regular expression to match only English letters (a-z, A-Z)
         filtered_text = re.sub(r'[^a-zA-Z]', '', substring)
         new_text = filtered_text.upper()
         return super(UpperCaseTextInput, self).insert_text(new_text, from_undo=from_undo)
 
 class NumericTextInput(TextInput):
     def insert_text(self, substring, from_undo=False):
-        # Filter out non-numeric characters and allow the minus sign for negative numbers
         if substring == "-" and not self.text:
             return super(NumericTextInput, self).insert_text(substring, from_undo=from_undo)
         elif (substring.startswith("-") and substring[1:].isdigit()) or substring.isdigit():
             return super(NumericTextInput, self).insert_text(substring, from_undo=from_undo)
         return False
 
-
-class MyBoxLayout(BoxLayout):
+class MyBoxLayout(BoxLayout):  # Change the inheritance here
     def __init__(self, cursor, **kwargs):
         super().__init__(**kwargs)
         self.cursor = cursor
-
-
-    TEMPERATURES_FLAPS15 = [-50, -40, -30, -20, -15, -10, -5, 0, 5, 10, 15, 20, 25, 30, 35, 40, 45]
-    TEMPERATURES_FLAPS5 = [-30, -25, -20, -15, -10, -5, 0, 5, 10, 15, 20, 25, 30, 35, 40, 45]
-    CALM_FLAPS15 = [17000, 25000]
-    HEADWIND_30_FLAPS15 = [17700, 26069]
-    CALM_FLAPS5 = [18000, 25000]
-    HEADWIND_30_FLAPS5 = [18750, 26041]
-    TAILWIND_5_FLAPS5 = [17750, 24458]
-
+        self.TEMPERATURES_FLAPS15 = [-50, -40, -30, -20, -15, -10, -5, 0, 5, 10, 15, 20, 25, 30, 35, 40, 45]
+        self.TEMPERATURES_FLAPS5 = [-30, -25, -20, -15, -10, -5, 0, 5, 10, 15, 20, 25, 30, 35, 40, 45]
+        self.CALM_FLAPS15 = [17000, 25000]
+        self.HEADWIND_30_FLAPS15 = [17700, 26069]
+        self.CALM_FLAPS5 = [18000, 25000]
+        self.HEADWIND_30_FLAPS5 = [18750, 26041]
+        self.TAILWIND_5_FLAPS5 = [17750, 24458]
 
     def find_temperature_range_flaps(self, temperature, flap_setting):
-        lower_temp_index = None
-        upper_temp_index = None
-        if flap_setting == 'Flaps 15':
-            temperatures = self.TEMPERATURES_FLAPS15
-        elif flap_setting == 'Flaps 5':
-            temperatures = self.TEMPERATURES_FLAPS5
-        else:
-            self.ids.label.text = "Invalid flap setting."
-            return
-
-        for i in range(len(temperatures) - 1):
-            if temperatures[i] <= temperature < temperatures[i + 1]:
-                if flap_setting == 'Flaps 15':
-                    lower_temp_index = i
-                    upper_temp_index = i + 1
-                else:
-                    lower_temp_index = i - 1 if i > 0 else i
-                    upper_temp_index = i
-                print(f"{flap_setting} lower_temp_index:{lower_temp_index} upper_temp_index:{upper_temp_index}")
+        temperatures = self.TEMPERATURES_FLAPS15 if flap_setting == 'Flaps 15' else self.TEMPERATURES_FLAPS5
+        for i, temp in enumerate(temperatures[:-1]):
+            if temp <= temperature < temperatures[i + 1]:
+                lower_temp_index = i if flap_setting == 'Flaps 5' else i - 1 if i > 0 else i
+                upper_temp_index = i + 1
                 return lower_temp_index, upper_temp_index
-        return lower_temp_index, upper_temp_index
+        return None, None
 
-    @staticmethod
-    def pressure_altitude_range(number):
+    def pressure_altitude_range(self, number):
         lower_alt = (number // 100) * 100
         upper_alt = ((number + 99) // 100) * 100
         return lower_alt, upper_alt
 
     def on_text_change(self, instance, value):
-        if len(value) > 4:
-            instance.text = value[:4]
+        instance.text = value[:4] if len(value) > 4 else value
 
-    def btn_pressed(self):
+    def calculate_delta_hPa(self):
         QNH_text = self.ids.qnh.text
-        if QNH_text:
-            QNH = int(QNH_text)
-            DELTA_hPa = 1013 - QNH
-        else:
-            DELTA_hPa = 0
+        return 1013 - int(QNH_text) if QNH_text else 0
 
-        TO_heading_text = self.ids.rw_heading.text
-        if TO_heading_text:
-            TO_heading = int(TO_heading_text)
-        else:
-            TO_heading = 0
-
-        wind_direction_text = self.ids.wind_direction.text
-        if wind_direction_text:
-            wind_direction = int(wind_direction_text)
-        else:
-            wind_direction = 0
-
-        wind_speed_text = self.ids.wind_speed.text
-        if wind_speed_text:
-            wind_speed = int(wind_speed_text)
-        else:
-            wind_speed = 0
-
-        # Calculate the relative wind angle
+    def calculate_wind_type_and_angle(self, TO_heading, wind_direction):
         wind_angle = (wind_direction - TO_heading) % 360
-
-        # Determine if it's headwind or tailwind
         if wind_angle <= 180:
-            if wind_angle <= 89:
-                wind_type = "Headwind"
-            else:
-                wind_type = "Tailwind"
-                wind_angle = 180 - wind_angle
+            wind_type = "Headwind" if wind_angle <= 89 else "Tailwind"
+            wind_angle = 180 - wind_angle
         else:
-            if wind_angle >= 271:
-                wind_type = "Headwind"
-                wind_angle = 360 - wind_angle
-            else:
-                wind_type = "Tailwind"
-                wind_angle = wind_angle - 180
+            wind_type = "Headwind" if wind_angle >= 271 else "Tailwind"
+            wind_angle = wind_angle - 180
+        return wind_angle, wind_type
 
+    def calculate_headwind_component(self, wind_speed, wind_angle_radians):
+        return wind_speed * abs(math.cos(wind_angle_radians))
+
+    def get_heading(self, heading_text):
+        try:
+            return int(heading_text)
+        except ValueError:
+            return 0
+
+    def calc_mtow_button(self):
+        DELTA_hPa = self.calculate_delta_hPa()
+        TO_heading = self.get_heading(self.ids.rw_heading.text)
+        wind_direction = int(self.ids.wind_direction.text) if self.ids.wind_direction.text else 0
+        wind_speed = int(self.ids.wind_speed.text) if self.ids.wind_speed.text else 0
+
+        wind_angle, wind_type = self.calculate_wind_type_and_angle(TO_heading, wind_direction)
         wind_angle_radians = math.radians(wind_angle)
-        headwind_component = wind_speed * math.cos(wind_angle_radians)
-        print(f"{wind_type}: {wind_angle} состовляющая продольная: {headwind_component}")
+        headwind_component = self.calculate_headwind_component(wind_speed, wind_angle_radians)
 
         APT_ID = self.ids.airport.text
         query = "SELECT * FROM airports WHERE ICAO = ?"
         self.cursor.execute(query, (APT_ID,))
         result = self.cursor.fetchone()
-        if result is not None:
+
+        if result:
             DELTA_m = DELTA_hPa * 8
             ALTITUDE_METERS = result[10] * 0.3048
             INFO = f"Аэропорт: {result[5]}\nГород: {result[6]},\nPressure Altitude: {int(ALTITUDE_METERS) + DELTA_m}"
@@ -139,10 +101,7 @@ class MyBoxLayout(BoxLayout):
             PRESSURE_ALTITUDE = 0
 
         temperature_text = self.ids.temperature.text
-        if temperature_text:
-            TEMPERATURE = int(temperature_text)
-        else:
-            TEMPERATURE = 15
+        TEMPERATURE = int(temperature_text) if temperature_text else 15
 
         flap_setting = self.ids.flap_spinner.text
         if flap_setting == 'Flaps 15':
@@ -155,7 +114,6 @@ class MyBoxLayout(BoxLayout):
             self.ids.label.text = "Invalid flap setting."
             return
 
-
         lower_temp_index, upper_temp_index = self.find_temperature_range_flaps(TEMPERATURE, flap_setting)
         if lower_temp_index is None or upper_temp_index is None:
             self.ids.label.text = "Температура за пределами значений"
@@ -165,62 +123,51 @@ class MyBoxLayout(BoxLayout):
         query = f"SELECT * FROM {table_name} WHERE pr_alt BETWEEN ? AND ?"
         self.cursor.execute(query, (lower, upper))
         MTOW_ROWS = self.cursor.fetchall()
-        MTOW_LOWER = None
-        MTOW_UPPER = None
-        for row in MTOW_ROWS:
-            pr_alt_value = row[0]
-            if pr_alt_value == lower:
-                MTOW_LOWER = row
-            elif pr_alt_value == upper:
-                MTOW_UPPER = row
+
+        MTOW_LOWER = next((row for row in MTOW_ROWS if row[0] == lower), None)
+        MTOW_UPPER = next((row for row in MTOW_ROWS if row[0] == upper), None)
 
         if MTOW_LOWER is None or MTOW_UPPER is None:
             self.ids.label.text = "Нет данных для указанного аэропорта"
             return
 
         ALT_FACTOR = (PRESSURE_ALTITUDE - lower) / 100
-        MTOW_LOW_TEMPT = int(MTOW_LOWER[lower_temp_index + 1] - ((MTOW_LOWER[lower_temp_index + 1] - MTOW_UPPER[lower_temp_index +1]) * ALT_FACTOR))
-        MTOW_UPPER_TEMPT = int(MTOW_LOWER[upper_temp_index + 1] - ((MTOW_LOWER[upper_temp_index + 1] - MTOW_UPPER[upper_temp_index +1]) * ALT_FACTOR))
+        MTOW_LOW_TEMPT = int(MTOW_LOWER[lower_temp_index + 1] - ((MTOW_LOWER[lower_temp_index + 1] - MTOW_UPPER[lower_temp_index + 1]) * ALT_FACTOR))
+        MTOW_UPPER_TEMPT = int(MTOW_LOWER[upper_temp_index + 1] - ((MTOW_LOWER[upper_temp_index + 1] - MTOW_UPPER[upper_temp_index + 1]) * ALT_FACTOR))
         DELTA_T_MEASURED = TEMPERATURE - TEMPERATURES[lower_temp_index]
         DELTA_T_SCALE = TEMPERATURES[upper_temp_index] - TEMPERATURES[lower_temp_index]
         TEMPERATURE_FACTOR = ((MTOW_LOW_TEMPT - MTOW_UPPER_TEMPT) / DELTA_T_SCALE) * DELTA_T_MEASURED
         CALM_MTOW = MTOW_LOW_TEMPT - TEMPERATURE_FACTOR
-        print(f"высота {PRESSURE_ALTITUDE}, диапазон: {lower} - {upper},\nТемература: {TEMPERATURE}")
-        print(f"{MTOW_LOWER[lower_temp_index + 1]} - {MTOW_LOWER[upper_temp_index + 1]}")
-        print(f"CALM_MTOW по фактической высоте: {MTOW_LOW_TEMPT} - {MTOW_UPPER_TEMPT}")
-        print(f"{MTOW_UPPER[lower_temp_index +1]} - {MTOW_UPPER[upper_temp_index +1]}")
-        print(f"CALM_MTOW: {CALM_MTOW}")
 
-        if flap_setting == 'Flaps 15':
-            if wind_type == 'Headwind':
-                HEADWIND_30_FLAPS15_MTOW = np.interp(CALM_MTOW, self.CALM_FLAPS15, self.HEADWIND_30_FLAPS15)
-                print(HEADWIND_30_FLAPS15_MTOW)
-                WIND_FACTOR = ((HEADWIND_30_FLAPS15_MTOW - CALM_MTOW) / 30) * headwind_component
-                MTOW = CALM_MTOW + WIND_FACTOR
-            else:
-                WIND_FACTOR = 100 * headwind_component
-                MTOW = CALM_MTOW - WIND_FACTOR
-        else:
-            if wind_type == 'Headwind':
-                HEADWIND_30_FLAPS5_MTOW = np.interp(CALM_MTOW, self.CALM_FLAPS5, self.HEADWIND_30_FLAPS5)
-                WIND_FACTOR = ((HEADWIND_30_FLAPS5_MTOW - CALM_MTOW) / 30) * headwind_component
-                MTOW = CALM_MTOW + WIND_FACTOR
-            else:
-                TAILWIND_5_FLAPS5_MTOW = np.interp(CALM_MTOW, self.CALM_FLAPS5, self.TAILWIND_5_FLAPS5)
-                WIND_FACTOR = ((CALM_MTOW - TAILWIND_5_FLAPS5_MTOW) / 5) * headwind_component
-                MTOW = CALM_MTOW - WIND_FACTOR
-
-
-        if MTOW > 25000:
-            MTOW = 25000
-
+        MTOW = self.calculate_final_mtow(CALM_MTOW, flap_setting, wind_type, headwind_component)
         INFO_FINAL = f"{INFO}\n{wind_type}: {round(headwind_component, 2)}\nMTOW:{int(MTOW)}"
         self.ids.label.text = INFO_FINAL
+        self.ids.tab2_label_mtow.text = f"MTOW: {str(int(MTOW))}"
 
-    def on_spinner_select(self, spinner, text):
+    def flaps_spinner_select(self, spinner, text):
         self.ids.label.text = f"{text}"
         print("Selected option:", text)
 
+    def calculate_final_mtow(self, CALM_MTOW, flap_setting, wind_type, headwind_component):
+        if flap_setting == 'Flaps 15':
+            if wind_type == 'Headwind':
+                WIND_FACTOR = ((np.interp(CALM_MTOW, self.CALM_FLAPS15, self.HEADWIND_30_FLAPS15) - CALM_MTOW) / 30) * headwind_component
+            else:
+                WIND_FACTOR = 100 * headwind_component
+        else:
+            if wind_type == 'Headwind':
+                WIND_FACTOR = ((np.interp(CALM_MTOW, self.CALM_FLAPS5, self.HEADWIND_30_FLAPS5) - CALM_MTOW) / 30) * headwind_component
+            else:
+                WIND_FACTOR = ((CALM_MTOW - np.interp(CALM_MTOW, self.CALM_FLAPS5, self.TAILWIND_5_FLAPS5)) / 5) * headwind_component
+
+        MTOW = CALM_MTOW + WIND_FACTOR if wind_type == 'Headwind' else CALM_MTOW - WIND_FACTOR
+        return min(MTOW, 25000)
+
+    def crew_onboard_spinner(self, spinner, text):
+        print("Selected option:", text)
+
+    def calc_free_button(self):
+        pass
 
 class MyApp(App):
     def __init__(self):
@@ -229,9 +176,7 @@ class MyApp(App):
         self.cursor = self.conn.cursor()
 
     def build(self):
-        box_layout = MyBoxLayout(cursor=self.cursor)
-        return box_layout
-
+        return MyBoxLayout(cursor=self.cursor)
 
 if __name__ == '__main__':
     MyApp().run()
